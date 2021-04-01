8517c813ef1a472fd2458d063c87f693f13e695d
Gonçalo Candeias Amaro <goncalo.cand.amaro@gmail.com>
2021-04-01 17:42:46 +0100

    refresh

---
+++ build/CMakeFiles/TEI_ImageMP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TEI_ImageMP.dir/jasnau.cpp.obj"
+  "TEI_ImageMP.exe"
+  "TEI_ImageMP.exe.manifest"
+  "TEI_ImageMP.pdb"
+  "libTEI_ImageMP.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TEI_ImageMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- jasnau.cpp
+++ jasnau.cpp
@@ -1,45 +1,44 @@
 #include <iostream>
 #include <omp.h>
 #include "CImg.h"
+
 using namespace std;
 using namespace cimg_library;
 
 int main() {
-    const char* fileName = "lena_gray_512.tif";
+    const char *fileName = "./lena_gray_512.tif";
     CImg<float> img = CImg<float>(fileName);
-    
+
     int colunas = img.width();
     int linhas = img.height();
     CImg<float> output = CImg<float>(colunas, linhas, 1, 1, 0.0);
-    
-    cout << colunas << endl; 
-    cout << linhas << endl; 
-    
+
+    cout << colunas << endl;
+    cout << linhas << endl;
+
     int k = 0;
-    #pragma omp parallel
+#pragma omp parallel
     {
-        while(k < 1000) {
-            for(int i = 0; i < colunas; i++)
-            {
-                for(int j = 0; j < linhas; j++)
-                {
+        while (k < 1000) {
+            for (int i = 0; i < colunas; i++) {
+                for (int j = 0; j < linhas; j++) {
                     float v = 0.0;
                     v = img(j, i) / 2;
                     output(j, i) = v;
-    //                 if(i == 300 && j == 256)
-    //                 {
-    //                     cout << output(j, i) << " " << v << endl;
-    //                 }
+                    //                 if(i == 300 && j == 256)
+                    //                 {
+                    //                     cout << output(j, i) << " " << v << endl;
+                    //                 }
                 }
             }
             k++;
         }
     }
     output.save("imagem1.pgm");
-    
-    cout << "Hello World" << endl; 
 
-     // cout << omp_get_thread_num() << endl;
- 
- return 0;
+    cout << "Hello World" << endl;
+
+    // cout << omp_get_thread_num() << endl;
+
+    return 0;
 }
